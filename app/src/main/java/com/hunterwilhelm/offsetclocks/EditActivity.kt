2f4ed82444b32f253b7a1a38fde7a96108e0826b
Hunter Wilhelm <yeskindofday@gmail.com>
2021-05-07 00:47:13 -0600

    Edit mode selection Hours, Minutes, Seconds

--- app/src/main/java/com/hunterwilhelm/offsetclocks/EditActivity.kt
+++ app/src/main/java/com/hunterwilhelm/offsetclocks/EditActivity.kt
@@ -4,13 +4,74 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
+enum class EditMode {
+    HOUR,
+    MINUTE,
+    SECOND
+}
+
 class EditActivity : AppCompatActivity() {
+    var editMode: EditMode = EditMode.SECOND
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        registerButtons()
+    }
+
+    private fun registerButtons() {
+        findViewById<Button>(R.id.edit_hour_button_inactive).setOnClickListener {
+            editMode = EditMode.HOUR
+            notifyEditModeChanged()
+        }
+        findViewById<Button>(R.id.edit_minute_button_inactive).setOnClickListener {
+            editMode = EditMode.MINUTE
+            notifyEditModeChanged()
+        }
+        findViewById<Button>(R.id.edit_second_button_inactive).setOnClickListener {
+            editMode = EditMode.SECOND
+            notifyEditModeChanged()
+        }
+    }
+
+    private fun notifyEditModeChanged() {
+        updateText()
+        updateButtons()
+    }
+
+    private fun updateText() {
+        findViewById<TextView>(R.id.edit_selected_mode_text).text = when (editMode) {
+            EditMode.SECOND -> resources.getString(R.string.selected_seconds)
+            EditMode.MINUTE -> resources.getString(R.string.selected_minutes)
+            EditMode.HOUR -> resources.getString(R.string.selected_hours)
+        }
+    }
+
+    private fun updateButtons() {
+        fun visibleTransform(visible: Boolean): Int {
+            return if (visible) View.VISIBLE else View.GONE
+        }
+
+        val editHourI = findViewById<Button>(R.id.edit_hour_button_inactive)
+        val editHour = findViewById<Button>(R.id.edit_hour_button)
+        val editSecondI = findViewById<Button>(R.id.edit_second_button_inactive)
+        val editSecond = findViewById<Button>(R.id.edit_second_button)
+        val editMinuteI = findViewById<Button>(R.id.edit_minute_button_inactive)
+        val editMinute = findViewById<Button>(R.id.edit_minute_button)
+
+        editHour.visibility = visibleTransform(editMode == EditMode.HOUR)
+        editHourI.visibility = visibleTransform(editMode != EditMode.HOUR)
+        editMinute.visibility = visibleTransform(editMode == EditMode.MINUTE)
+        editMinuteI.visibility = visibleTransform(editMode != EditMode.MINUTE)
+        editSecond.visibility = visibleTransform(editMode == EditMode.SECOND)
+        editSecondI.visibility = visibleTransform(editMode != EditMode.SECOND)
     }
 
     override fun onBackPressed() {
